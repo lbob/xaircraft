0724edf0dd24ac1fd63d846e798e4d8515c5f6db
lbob <skyweo@foxmail.com>
2015-12-29 14:20:03 +0800

    增加DistinctFieldFunction函数

---
+++ src/Database/Func/DistinctFieldFunction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lbob
+ * Date: 2015/12/29
+ * Time: 14:18
+ */
+
+namespace Xaircraft\Database\Func;
+
+
+use Xaircraft\Database\FieldInfo;
+use Xaircraft\Database\QueryContext;
+
+class DistinctFieldFunction extends FieldFunction
+{
+
+    public function getString(QueryContext $context)
+    {
+        if ("*" !== $this->field) {
+            $field = FieldInfo::make($this->field);
+            return "DISTINCT(" . $field->getName($context) . ")";
+        }
+        return "DISTINCT(*)";
+    }
+}
