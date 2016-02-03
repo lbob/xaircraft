f4520faaf0d72d95426f145683ae5c7cf89521de
lbob <skyweo@foxmail.com>
2016-02-03 13:59:16 +0800

    规范身份验证相关异常状态码

--- app/test/TableQueryTest.php
+++ app/test/TableQueryTest.php
@@ -1,4 +1,5 @@
 <?php
+use Xaircraft\Database\WhereQuery;
 use Xaircraft\DB;
 
 /**
@@ -17,6 +18,17 @@ class TableQueryTest extends PHPUnit_Framework_TestCase
     public function testWhereCondition()
     {
         $query = DB::table('user')->where('id', '<>', 0)->select();
+        var_dump($query->getQueryString());
+        ob_flush();
+    }
+
+    public function testWhereQuery()
+    {
+        $query = DB::table('user')->select(array(
+            'ids' => function (WhereQuery $whereQuery) {
+                $whereQuery->select('id')->from('product_definition')->take();
+            }
+        ));
         var_dump($query->getQueryString());
         ob_flush();
     }
